f20f6e89f8fa06560daee16f7323879058100366
ConfusedKnight <aarushdangol2006@gmail.com>
2025-03-13 14:12:39 +0545

    fixed responsive padding

--- frontend/src/app/(pages)/BenefitsPage.tsx
+++ frontend/src/app/(pages)/BenefitsPage.tsx
@@ -15,7 +15,7 @@ export default function BenefitsPage() {
           className="absolute right-3 top-46 hidden xl:block -rotate-12"
         />
       </div>
-      <div className={`${cabinetExtraBold.className}  headings text-main-title`}>
+      <div className={`${cabinetExtraBold.className}  headings`}>
         Benefits of Participating
       </div>
       <div className="flex justify-center">
--- frontend/src/app/components/BenefitsOfParticipating/CardDark.tsx
+++ frontend/src/app/components/BenefitsOfParticipating/CardDark.tsx
@@ -22,7 +22,7 @@ export default function CardDark({
     <div className=" primary-border-background lg:w-[38rem] lg:h-[21.19rem] md:w-[36rem] w-full h-fit rounded-[1.875rem] p-[0.1875rem] relative ">
       <div className=" new-gradient-background w-full lg:h-full md:h-fit rounded-[1.875rem] flex flex-col px-12 pt-9 pb-10 lg:gap-7 justify-start md:pt-14 relative">
         <div
-          className={`${cabinetExtraBold.className} text-primary-gradient-color text-[1.25rem] md:text-[2.25rem] w-[10.69rem] md:w-[18.72rem] md:leading-[2.75rem] pb-0 font-extrabold z-10`}
+          className={`${cabinetExtraBold.className} text-primary-gradient-color text-[1.25rem] md:text-[2.25rem] w-[10.69rem] md:w-[18.72rem] md:leading-[2.75rem] pb-[1.5rem] md:pb-0 font-extrabold z-10 `}
         >
           {title}
         </div>
