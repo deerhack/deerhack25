b44c6cf8c00c5bb562c44ed79cd22093e9e99fdc
ConfusedKnight <aarushdangol2006@gmail.com>
2025-04-04 21:29:23 +0545

    fix

--- frontend/src/app/components/GoBack/GoBackToHome.tsx
+++ frontend/src/app/components/GoBack/GoBackToHome.tsx
@@ -2,12 +2,13 @@ import { cabinetExtraBold } from "@/app/utils/fonts";
 import Link from "next/link";
 const GoBackToHome = () => {
   return (
-    <div>
+    <div className={`bg-secondary w-48 ${cabinetExtraBold.className} text-center py-3 px-4 rounded-[0.231rem] w-[6.969rem] md:w-[14.202rem] h-[1.7rem] md:h-[3.445rem]`}
+    >
       <a
-        className={`bg-secondary w-48 ${cabinetExtraBold.className} text-center py-3 px-4 text-lg rounded-md`}
+        className={`text-[0.7rem] md:text-[1.5rem] md:font-black absolute left-[7.5rem] md:left-[17rem] bottom-[0.7rem] md:bottom-[2rem]`}
         href="/"
       >
-        Go Back to home
+        Go Back to Home
       </a>
     </div>
   );
--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -5,35 +5,51 @@ import styles from "./styles.module.css";
 import { cabinetBlack, cabinetBold, cabinetExtraBold } from "./utils/fonts";
 import { redirect } from "next/navigation";
 import GoBackToHome from "./components/GoBack/GoBackToHome";
+import Image from "next/image";
+import darin_404 from "@/app/assets/images/darin_404.svg";
 
 export default function NotFound() {
   return (
+
     <div
-      className={`${styles.background_container} h-screen flex justify-center`}
+      className={`${styles.background_container} relative`}
     >
-      <div
-        className={`${styles.muted_color} rounded-[0.65rem] w-[80vw] md:w-[50vw] max-w-[1080px] h-fit lg:h-[24vh] flex flex-col gap-4 justify-center items-center  border-secondary border-2 backdrop-blur-sm px-2 py-6 mx-auto absolute top-[60%]`}
+      <div className="sm:flex sm:justify-center sm:items-center">
+       <div
+        className={`${styles.muted_color} rounded-[0.65rem] w-[20.409rem] md:w-[45.792rem] max-w-[1080px] h-[5.531rem] md:h-fit lg:h-[12.006rem]
+           flex flex-col gap-4 justify-center items-center  border-secondary border-2 backdrop-blur-sm px-2 py-6 mx-auto
+            relative top-[24rem] md:top-[38rem] left-[0.7rem] md:left-[15rem]`}
       >
         <p
-          className={`${cabinetExtraBold.className} text-primary-gradient-color-reverse w-fit text-5xl text-center`}
+          className={`${cabinetExtraBold.className} text-[#F5C144] w-fit text-[1.25rem] md:text-[3rem] text-center`}
         >
-          Nobody Seems to be here!
+          Nobody seems to be here!
         </p>
         <GoBackToHome />
+      </div> 
       </div>
+
+      <Image
+      src={darin_404}
+      alt="darin"
+      className="absolute top-[9rem] left-[7.5rem] hidden lg:block"
+      >
+
+      </Image>
     </div>
+
   );
 }
 
 {
   /* <div
-      className={`${styles.background_container} h-screen flex justify-center`}
+      className={${styles.background_container} h-screen flex justify-center}
     >
       <div
-        className={`${styles.muted_color} rounded-[0.65rem] w-[80vw] md:w-[50vw] h-fit lg:h-[24vh] flex flex-col gap-4 justify-center items-center  border-secondary border-2 backdrop-blur-sm px-2 py-6 mx-auto absolute top-[60%]`}
+        className={${styles.muted_color} rounded-[0.65rem] w-[80vw] md:w-[50vw] h-fit lg:h-[24vh] flex flex-col gap-4 justify-center items-center  border-secondary border-2 backdrop-blur-sm px-2 py-6 mx-auto absolute top-[60%]}
       >
         <p
-          className={`${cabinetExtraBold.className} text-primary-gradient-color-reverse w-fit text-5xl text-center`}
+          className={${cabinetExtraBold.className} text-primary-gradient-color-reverse w-fit text-5xl text-center}
         >
           Nobody Seems to be here!
         </p>
