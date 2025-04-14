0fc00afdfc3bdea0e4bbea0d1a583c804f75ebe9
Supritphuyal <supritphuyal123@gmail.com>
2025-04-14 22:47:15 +0545

    Fixed: responsiveness of prizes section

--- frontend/src/app/components/GrandPrize/GrandPrizeCard.tsx
+++ frontend/src/app/components/GrandPrize/GrandPrizeCard.tsx
@@ -1,71 +1,34 @@
-import { ReactElement } from "react";
-import Trophy from "@/app/assets/images/trophy";
-import Leaf from "@/app/assets/images/Leaf";
-import {
-  satoshiRegular,
-  cabinetBold,
-  cabinetBlack,
-  cabinetExtraBold,
-} from "@/app/utils/fonts";
+import type { ReactElement } from "react"
+import Trophy from "@/app/assets/images/trophy"
+import Leaf from "@/app/assets/images/Leaf"
+import { satoshiRegular, cabinetBlack, cabinetExtraBold } from "@/app/utils/fonts"
 
 export default function GrandPrizeCard(): ReactElement {
   return (
-    
-    <div className="flex flex-col justify-evenly grow h-[29.448rem] md:h-[43.284rem] rounded-[2rem] primary-aboutpage-background w-[19.966rem] md:w-[29.348rem]">
+    <div className="flex flex-col justify-evenly grow lg:h-[43.448rem] h-[29.448rem] sm:h-[29.448rem] md:h-[32.448rem] rounded-[2rem] primary-aboutpage-background w-[19.966rem] sm:w-[19.966rem] md:w-[23.966rem] lg:w-[29.348rem]">
       <div className="flex flex-col text-left ml-[2rem]">
         <div className="flex">
           <Trophy height={160} width={160} />
         </div>
-        {/* <p className={`${cabinetExtraBold.className} font-extrabold text-3xl sm:text-4xl lg:text-5xl text-[#110C24]`}> */}
-        <p className={`${cabinetExtraBold.className} font-extrabold text-3xl md:text-6xl text-[#110C24]`}>
+        <p
+          className={`${cabinetExtraBold.className} font-extrabold text-3xl sm:text-3xl md:text-4xl lg:text-6xl text-[#110C24]`}
+        >
           Grand Prize
         </p>
       </div>
-      
-      {/* <p className={`${cabinetBlack.className} text-5xl sm:text-6xl lg:text-7xl font-extrabold text-left ml-10 mt-28 relative`}> */}
-      <p className={`${cabinetBlack.className} text-5xl font-extrabold text-left ml-10 mt-28 relative`}>
-        Rs.<br/> 50,000
-        <div 
-          // className={`${satoshiRegular.className} text-sm sm:text-base lg:text-lg italic text-left`}
-          className={`${satoshiRegular.className} text-sm italic text-left`}
-        > 
+
+      <p
+        className={`${cabinetBlack.className} text-5xl sm:text-5xl md:text-6xl lg:text-7xl font-extrabold text-left ml-10 mt-28 relative`}
+      >
+        Rs.
+        <br /> 50,000
+        <div className={`${satoshiRegular.className} text-sm sm:text-sm md:text-lg lg:text-2xl italic text-left`}>
           to overall winner
         </div>
-        
-    
-        <div className="absolute bottom-[4rem]">
-        
-          <Leaf height={300} width={300}/>
+        <div className="absolute bottom-[-6rem] right-0">
+          <Leaf height={300} width={300} />
         </div>
       </p>
     </div>
-    // <div
-    //   className={`flex flex-col justify-evenly grow h-[43.284rem] sm:h-72 md:h-72 lg:h-[25rem] w-[29.348rem] lg:w-[22rem] primary-aboutpage-background rounded-2xl`}
-    // >
-    //   <div className="flex flex-col justify-left text-left  ml-[2rem] ">
-    //     <div className="flex justify-left">
-    //       <Trophy height={164} width={164} />
-    //     </div>
-    //     <p
-    //       className={`${cabinetExtraBold.className} font-extrabold  text-4xl text-[#110C24] sm:text-xl lg:text-5xl`}
-    //     >
-    //       Grand Prize
-    //     </p>
-    //   </div>
-      
-    //   <p
-    //     className={`${cabinetBlack.className} text-6xl sm:text-4xl lg:text-6xl font-extrabold text-left ml-[2rem] mt-[2rem]`}
-    //   >
-    //     Rs.<br/> 50,000
-    //   <div 
-    //   className={`${satoshiRegular.className} text-sm sm:text-xs lg:text-base font-style: italic text-left`}
-    //   > 
-    //   to overall winner
-    //   </div>
-    //   </p>
-
-    // </div>
-    // <div className="flex flex-col justify-evenly grow h-[36.784rem] w-[38.348rem]  md:max-h-[36.784rem] md:max-w-[18rem] rounded-[2rem] primary-aboutpage-background">
-
-  );
+  )
 }
--- frontend/src/app/components/OtherPrizes/SwagPrizes.tsx
+++ frontend/src/app/components/OtherPrizes/SwagPrizes.tsx
@@ -1,49 +1,21 @@
-import { ReactElement } from "react";
-import {
-  cabinetBlack,
-  cabinetBold,
-  cabinetExtraBold,
-  cabinetMedium,
-  cabinetRegular,
-} from "@/app/utils/fonts";
-
-import thumbsupSVG from "@/app/assets/images/ThumbsUp";
+import type { ReactElement } from "react"
+import { cabinetExtraBold } from "@/app/utils/fonts"
 
 export default function Swagprizes({
   svg,
 }: {
-  svg: ReactElement;
+  svg: ReactElement
 }) {
   return (
     <div>
-        {/* <div className="bg-violet flex flex-col h-[25.25rem] w-[17.474rem] max-w-[17.5rem] p-4 sm:p-6 rounded-[2rem] justify-between py-6 sm:py-10 items-center"> */}
-        <div className="bg-violet flex flex-col h-fit md:h-[29.041rem] w-[20.027rem] max-w-[23.5rem] rounded-[2rem] justify-between py-6 items-center ">
-          {/* <div className={`${cabinetExtraBold.className} text-primary-gradient-color text-xl sm:text-2xl lg:text-4xl text-center`}>           */}
-            <div className={` ${cabinetExtraBold.className} text-center  text-primary-gradient-color h-[7.875rem] w-[10.375rem] leading-none text-[1.701rem] md:text-[2.5rem] py-4  md:w-[20rem] md:items-center`}>          
-              Swags and Freebies!
-            </div>
-  
-          {/* <div className="flex justify-center w-full max-w-[10rem] sm:max-w-[12rem] lg:max-w-[14rem]">{svg}</div> */}
-          <div className="flex justify-center w-full h-fit py-4">{svg}</div>
+      <div className="bg-violet flex flex-col h-fit sm:h-fit md:h-fit lg:h-[29.041rem] w-[20.027rem] sm:w-[20.027rem] md:w-[24.027rem] max-w-[23.5rem] rounded-[2rem] justify-between py-6 items-center">
+        <div
+          className={`${cabinetExtraBold.className} text-center text-primary-gradient-color h-[7.875rem] w-[10.375rem] leading-none text-[1.701rem] sm:text-[1.701rem] md:text-[2.01rem] lg:text-[2.5rem] py-4 sm:w-[10.375rem] md:w-[10.375rem] lg:w-[20rem] lg:items-center`}
+        >
+          Swags and Freebies!
         </div>
+        <div className="flex justify-center w-full h-fit py-4">{svg}</div>
       </div>
-    
-    );
-    //   <div className="flex justify-center items-center">
-    
-  //   <div className="bg-violet flex flex-col h-[28.25rem] w-[17.474rem] p-[2px] rounded-[1rem] justify-between py-12">
-  //     <div
-  //       className={`${cabinetExtraBold.className} text-primary-gradient-color text-2xl sm:text-2xl lg:text-4xl text-center sm:text-center`}
-  //     >
-  //       Swags and <br className="hidden sm:block" />
-  //       Freebies!
-  //     </div>
-  //     <div className="flex justify-center">
-      
-  //     {svg}
-      
-  //     </div>
-  //   </div>
-  // </div>
-
+    </div>
+  )
 }
