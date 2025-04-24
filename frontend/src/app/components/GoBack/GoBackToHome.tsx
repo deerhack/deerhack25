3af0e90877864230a9a12b13c2d823bbb7da5b2e
ConfusedKnight <aarushdangol2006@gmail.com>
2025-04-24 23:22:11 +0545

    fix: fixed desktop andmobile responsiveness

--- frontend/src/app/components/GoBack/GoBackToHome.tsx
+++ frontend/src/app/components/GoBack/GoBackToHome.tsx
@@ -2,10 +2,10 @@ import { cabinetExtraBold } from "@/app/utils/fonts";
 import Link from "next/link";
 const GoBackToHome = () => {
   return (
-    <div className={`bg-secondary w-48 ${cabinetExtraBold.className} text-center py-3 px-4 rounded-[0.231rem] w-[6.969rem] md:w-[14.202rem] h-[1.7rem] md:h-[3.445rem]`}
+    <div className={`bg-secondary w-48 ${cabinetExtraBold.className} text-center py-3 px-4 rounded-[0.231rem] w-[8rem] md:w-[12.651rem] h-[1.7rem] md:h-[3.107rem] flex justify-center items-center`}
     >
       <a
-        className={`text-[0.7rem] md:text-[1.5rem] md:font-black absolute left-[7.5rem] md:left-[17rem] bottom-[0.7rem] md:bottom-[2rem]`}
+        className={`text-[0.75rem] md:text-[1.232rem] md:font-black`}
         href="/"
       >
         Go Back to Home
--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -8,35 +8,37 @@ import { redirect } from "next/navigation";
 import GoBackToHome from "./components/GoBack/GoBackToHome";
 import Image from "next/image";
 import darin_404 from "@/app/assets/images/darin_404.svg";
+import Navbar from "./components/Navbar/Navbar";
 
 export default function NotFound() {
   return (
 
     <div
-      className={`${styles.background_container} relative`}
+      className={`${styles.background_container} overflow-hidden`}
     >
+      <Navbar/>
       <div className="sm:flex sm:justify-center sm:items-center">
        <div
-        className={`${styles.muted_color} rounded-[0.65rem] w-[20.409rem] md:w-[45.792rem] max-w-[1080px] h-[5.531rem] md:h-fit lg:h-[12.006rem]
+        className={`rounded-[0.65rem] w-[20.409rem] md:w-[40.808rem] max-w-[1080px] h-[5.531rem] md:h-[10.705rem]
            flex flex-col gap-4 justify-center items-center  border-secondary border-2 backdrop-blur-sm px-2 py-6 mx-auto
-            relative top-[24rem] md:top-[38rem] left-[0.7rem] md:left-[15rem]`}
+            relative top-[20rem] md:top-[34rem] left-[0.7rem] md:left-[14rem]`}
       >
         <p
-          className={`${cabinetExtraBold.className} text-[#F5C144] w-fit text-[1.25rem] md:text-[3rem] text-center`}
+          className={`${cabinetExtraBold.className} text-[#F5C144] w-fit text-[1.25rem] md:text-[2.674rem] text-center`}
         >
           Nobody seems to be here!
         </p>
         <GoBackToHome />
       </div> 
       </div>
-
-      <Image
+          
+      {/* <Image
       src={darin_404}
       alt="darin"
-      className="absolute top-[9rem] left-[7.5rem]"
+      className="absolute top-[5rem] left-[7.5rem] h-[40rem] overflow-hidden"
       >
 
-      </Image>
+      </Image> */}
     </div>
 
   );
