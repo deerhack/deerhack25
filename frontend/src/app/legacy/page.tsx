0b12be845cbf60d015f1b0e87a24ac1b90c2e590
VaidyaIshan <165746811+VaidyaIshan@users.noreply.github.com>
2025-05-14 22:42:08 +0545

    Second

---
+++ frontend/src/app/legacy/page.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
+import Image from "next/image";
+import LegacyFlower from "../assets/images/LegacyFlower.svg";
+import LegacyLeafLeft from "../assets/images/LegacyLeafLeft.svg";
+import LegacyLeafRight from "../assets/images/LegacyLeafRight.svg";
+import { LegacySection } from "../sections/LegacySection";
+import { HistorySection } from "../sections/HistorySection";
+export default function LegacyPage(){
+    return(
+        <>
+        <Navbar/>
+        <div className="relative overflow-hidden flex items-center justify-center w-full  mx-auto">
+        <LegacySection/>
+
+        </div>
+        <div>
+          <HistorySection/>
+        </div>
+        <Footer/>
+      
+        
+        </>
+    )
+}
