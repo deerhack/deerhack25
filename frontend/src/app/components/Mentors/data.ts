340bba62018dc62ac28642b9a88fe2c017a36fc2
Srijan Dahal <srijan102dahal@gmail.com>
2025-07-02 22:37:00 +0545

    fix: mentors pictures and footer instagram link

--- frontend/src/app/components/Mentors/data.ts
+++ frontend/src/app/components/Mentors/data.ts
@@ -38,55 +38,61 @@ const Mentors_value: Mentors[] = [
     name: "Bishal Joshi",
     position: "Software Engineer",
     linkedin_url: "https://www.linkedin.com/in/bishal-joshi-7a3711203",
-    image: "https://ghostuf.github.io/static_images/bishal_joshi.png",
+    image: "https://ghostuf.github.io/static_images/bishal_joshi.jpeg",
   },
   {
     name: "Toshika Ojha",
     position: "Client Success Consultant",
     linkedin_url: "https://www.linkedin.com/in/toshikaojha/",
-    image:"https://ghostuf.github.io/static_images/toshika_ojha.png"
+    image:"https://ghostuf.github.io/static_images/toshika_ojha.jpg"
   },
   {
     name: "Luja Chitrakar",
     position: "Clock B Tech",
     linkedin_url: "https://www.linkedin.com/in/luja-chitrakar/",
-    image:"https://ghostuf.github.io/static_images/luja_chitrakar.png"
+    image:"https://ghostuf.github.io/static_images/luja_chitrakar.jpeg"
   },
   {
     name: "Mala Deep Upadhaya",
     position: "Data Consultant - Dataksha Analytics",
     linkedin_url: "https://www.linkedin.com/in/maladeep",
-    image: "https://ghostuf.github.io/static_images/mala_deep_upadhaya.png"
+    image: "https://ghostuf.github.io/static_images/mala_deep_upadhyaya.jpg"
   },
+  // {
+  //   name: "Elisha Baniya",
+  //   position: "QA - Maitri Services",
+  //   linkedin_url: "https://www.linkedin.com/in/elishabaniya",
+  //   image: "https://ghostuf.github.io/static_images/elisha_baniya.jpg"
+  // },
   {
-    name: "Elisha Baniya",
-    position: "QA - Maitri Services",
-    linkedin_url: "https://www.linkedin.com/in/elishabaniya",
-    image: "https://ghostuf.github.io/static_images/elisha_baniya.png"
+    name: "Yashaswee Sakha",
+    position: "UI/UX Engineer, Deerhold Ltd.",
+    linkedin_url: "https://www.linkedin.com/in/yashaswee-sakha/",
+    image: "https://ghostuf.github.io/static_images/yashaswee_sakha.jpg"
   },
   {
     name: "Mamata Maharjan",
     position: "Senior Software Engineer",
     linkedin_url: "https://www.linkedin.com/in/mamata-maharjan-b4197b178",
-    image: "https://ghostuf.github.io/static_images/mamata_maharjan.png"
+    image: "https://ghostuf.github.io/static_images/mamata_maharjan.jpg"
   },
   {
     name: "Sushan Shakya",
     position: "Full Stack Developer - RIMES",
     linkedin_url: "https://www.linkedin.com/in/sushan-shakya-8953711a6/",
-    image: "https://ghostuf.github.io/static_images/sushan_shakya.png"
+    image: "https://ghostuf.github.io/static_images/sushan_shakya.jpg"
   },
   {
     name: "Prashant Malla",
     position: "Embedded System Engineer - Truenary Solutions",
     linkedin_url: "https://www.linkedin.com/in/prashant-malla-139339b2",
-    image: "https://ghostuf.github.io/static_images/prashant_malla.png"
+    image: "https://ghostuf.github.io/static_images/prashant_malla.jpg"
   },
   {
     name: "Saru Manandhar",
     position: "Software Engineer - Leapfrog Technology",
     linkedin_url: "https://www.linkedin.com/in/saru-manandhar-71222614a",
-    image: "https://ghostuf.github.io/static_images/saru_manandhar.png"
+    image: "https://ghostuf.github.io/static_images/saru_manandhar.jpg"
   },
   // {
   //   name: "Labbi Karmacharya",
@@ -97,8 +103,8 @@ const Mentors_value: Mentors[] = [
   {
     name: "Rojan Upreti",
     position: "Machine Learning Engineer",
-    linkedin_url: "", // No LinkedIn URL provided
-    image: "https://ghostuf.github.io/static_images/rojan_upreti.png"
+    linkedin_url: "https://www.linkedin.com/in/rojanupreti/", // No LinkedIn URL provided
+    image: "https://ghostuf.github.io/static_images/rojan_upreti.jpg"
   },
 ]
 
